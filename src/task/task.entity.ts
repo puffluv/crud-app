f7233ef8de4124d180fe9c6dd7448f93cfa900c0
puffluv <alibihasenov@gmail.com>
2024-07-24 23:34:01 +0700

    Add: task status, tags, createdAt, updateAt

--- src/task/task.entity.ts
+++ src/task/task.entity.ts
@@ -1,10 +1,14 @@
-import { ITask } from './task.interface';
+import { ITask, Status } from './task.interface';
 
 export class Task implements ITask {
-  id: number;
+  id = new Date().getTime();
   task: string;
+  status: Status = Status.CREATED;
+  tags: string[] = [];
+  createdAt: Date = new Date();
+  updateAt: Date = new Date();
+
   constructor(task: string) {
     this.task = task;
-    this.id = new Date().getTime();
   }
 }
--- src/task/task.interface.ts
+++ src/task/task.interface.ts
@@ -1,4 +1,16 @@
+export enum Status {
+  CREATED = 'created',
+  PROCESSING = 'processing',
+  ABORTED = 'aborted',
+  ERROR = 'error',
+  DONE = 'done',
+}
+
 export interface ITask {
   id: number;
   task: string;
+  status: Status;
+  tags: string[];
+  createdAt: Date;
+  updateAt: Date;
 }

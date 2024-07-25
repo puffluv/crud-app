fb4936aa9f38980b98c31e63bbe8ba0d4a4c4d70
puffluv <alibihasenov@gmail.com>
2024-07-25 15:56:58 +0700

    Fixed and added uuid

--- src/task/task.entity.ts
+++ src/task/task.entity.ts
@@ -1,14 +1,17 @@
 import { ITask, Status } from './task.interface';
+import { v4 as uuidv4 } from 'uuid';
 
 export class Task implements ITask {
-  id = new Date().getTime();
+  id: string = uuidv4();
   task: string;
   status: Status = Status.CREATED;
   tags: string[] = [];
   createdAt: Date = new Date();
   updateAt: Date = new Date();
 
-  constructor(task: string) {
+  constructor(task: string, tags?: string[], status?: Status) {
     this.task = task;
+    this.tags = tags;
+    this.status = status;
   }
 }

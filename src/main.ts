7b9266d1275ba969545868f74cc2fb82e22fd862
puffluv <alibihasenov@gmail.com>
2024-07-25 21:49:46 +0700

    Global filters added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './exception-filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionFilter);
   await app.listen(3000);
 }
 bootstrap();
